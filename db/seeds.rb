27bc5e7fd2a7bd2110668df4d52354cf3a337e2a
HaiLeader <pham.haisv@gmail.com>
2017-05-05 14:27:42 +0700

    Nothing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create(:title => 'Programing Ruby',
+               :description =>
+                   %{<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.</p>},
+              :image_url => 'https://img.clipartfest.com/f300bb41331a1eb8444de6975317cc31_flame-mc-community-forum-minecraft-server-clipart-64x64_256-256.png',
+              :price => 49.5)
