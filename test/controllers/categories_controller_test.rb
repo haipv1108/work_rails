2b4eceb5e01af4553bb484435abc59ede7b74de4
HaiLeader <pham.haisv@gmail.com>
2017-05-05 14:31:26 +0700

    merged develop branch

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class CategoriesControllerTest < ActionController::TestCase
+  def setup
+    @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "123456", admin: true)
+  end
+
+  test "should get categories index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get new" do
+    session[:user_id] = @user.id
+    get :new
+    assert_response :success
+  end
+
+  test "should get show" do
+    get(:show, {'id' => @category.id})
+    assert_response :success
+  end
+end
